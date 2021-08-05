83a6330baeaebfb89de06a8f8262c9375a21e1bb
Imbluedabadi <imbluedabadi1969@gmail.com>
2021-08-05 10:27:09 +0100

    index created

---
+++ wp-content/themes/loja-legal/index.php
@@ -0,0 +1,90 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
+    <title>Loja Legal!</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-dark bg-dark">
+    <a class="navbar-brand text-center">Loja Legal</a>
+    <img  class="navbar-brand" src="https://picsum.photos/40/40" alt="">
+    <button type="button" class="btn btn-primary me-md-2">Login</button>
+    </nav>
+    <br>
+    <h3 class="text-center" >Automoveis</h3>
+    <button type="button" class="btn btn-primary me-md-2">Venda o seu carro</button>
+    <br>
+    
+    <div class="card-group">
+    <div class="card" style="width: 18rem;">
+        <img src="https://picsum.photos/300/200" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="text-center card-title">Mercedes Classe A</h5>
+            <p class="text-center card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Detalhes</a>
+        </div>
+    </div>
+    <div class="card" style="width: 18rem;">
+        <img src="https://picsum.photos/300/201" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="text-center card-title">Mercedes Classe A</h5>
+            <p class="text-center card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Detalhes</a>
+        </div>
+    </div>
+    <div class="card" style="width: 18rem;">
+        <img src="https://picsum.photos/300/202" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="text-center card-title">Mercedes Classe A</h5>
+            <p class="text-center card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Detalhes</a>
+        </div>
+    </div> 
+    </div>
+    <div class="card-group">
+        <div class="card" style="width: 18rem;">
+            <img src="https://picsum.photos/300/203" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="text-center card-title">Mercedes Classe A</h5>
+                <p class="text-center card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Detalhes</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="https://picsum.photos/300/204" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="text-center card-title">Mercedes Classe A</h5>
+                <p class="text-center card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Detalhes</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="https://picsum.photos/300/205" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="text-center card-title">Mercedes Classe A</h5>
+                <p class="text-center card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Detalhes</a>
+            </div>
+        </div> 
+    </div>
+    <br>
+    <footer>
+        <div class="FooterWrapper text-center">
+             &copy; by Fred
+        </div>
+        <br>
+    </footer>
+
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+
+  </body>
+</html>
