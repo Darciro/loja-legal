785027af8ed1fd48093f890987e54b99da2a81c0
Imbluedabadi <imbluedabadi1969@gmail.com>
2021-08-05 16:39:38 +0100

    Single Created

--- wp-content/themes/loja-legal/functions.php
+++ wp-content/themes/loja-legal/functions.php
@@ -4,6 +4,11 @@ add_theme_support('post-thumbnails');
 
 
 function my_excerpt_length($length){
-    return 10;
+    return 9;
 }
 add_filter('excerpt_length', 'my_excerpt_length');
+
+function my_content_length($length){
+    return 9;
+}
+add_filter('content_length', 'my_content_length');
